8ec3a5742f17a703a6d57f5d18bbcc3eb2db6213
Isabel I. Veldhuizen <is.veldhuizen@belastingdienst.nl>
2026-02-03 16:09:51 +0100

    Dictionary aangemaakt om verschillende soorten topics aan te kunnen maken (procespagina, stap, taak)

--- src/askdelphi/topictools.py
+++ src/askdelphi/topictools.py
@@ -1,15 +1,24 @@
 from typing import Optional
 from askdelphi.authentication import AskDelphiClient
 
+TOPIC_TYPE_IDS = {
+            "Digitale Coach Procespagina": "7d332fbb-44f5-469f-b570-874e701e526b",
+            "Stap": "c568af9a-6c89-45cf-a580-bc94e1c62ae3",
+            "Taak": "6aba8437-c8df-42d2-a868-840847c124ca"
+        }
+
 class TopicTools:
     def __init__(self, client: AskDelphiClient):
         self.client = client
 
     def topic_upload(self, topicTitle: str, topicTypeName: str):
-        if topicTypeName == "Digitale Coach Procespagina":
-            topicTypeId = "7d332fbb-44f5-469f-b570-874e701e526b"
-        else:
-            raise ValueError(f"Unknown topic type: {topicTypeName}")
+        topicTypeId = TOPIC_TYPE_IDS.get(topicTypeName)
+        
+        if topicTypeId is None:
+            raise ValueError(
+                f"Unknown topic type: '{topicTypeName}'."
+                f"Available types: {list(TOPIC_TYPE_IDS.keys())}"
+            )
         
         endpoint = "v4/tenant/{tenantId}/project/{projectId}/acl/{aclEntryId}/topic"
         data = {
