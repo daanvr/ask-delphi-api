105ba3c0155809a7e3bbad874dd31c777b603088
Arie A. Baas <a_baas@belastingdienst.nl>
2026-02-12 15:23:06 +0100

    Demo ready gemaakt aanleggen digicoach

--- ask-delphi-api/src/ask_delphi_api/import_digicoach.py
+++ ask-delphi-api/src/ask_delphi_api/import_digicoach.py
@@ -11,7 +11,7 @@ class Import:
     TASK_NAME = "Taak"
     ACTION_NAME = "Stap"
 
-    def __init__(self, client: AskDelphiClient):
+    def __init__(self):
 
         self.client = AskDelphiClient()
         self.client.authenticate()   # pakt automatisch portal code uit .env
@@ -19,6 +19,9 @@ class Import:
         self.project = Project(self.client)
         self.topic = TopicTools(self.client, self.project)
         self.relation = Relation(self.client)
+
+    def _get_topic(self):
+        return self.topic
         
     # Create Voorgedefinieerde zoekopdracht topic
     def create_voorgedefinieerde_zoekopdracht_topic(self, name: str) -> str:
@@ -41,5 +44,58 @@ class Import:
         return topic_id_digicoach, topic_version_id_digicoach
     
     # Tag Digitale Coach Procespagina
-    # def add_tag():
-    #     relation.add_tag(topic_id_digicoach, topic_version_id_digicoach, "interactie")
+    def add_tag(self, topic_id_digicoach: str, topic_version_id_digicoach: str, tag: str):
+        # self.topic.checkout(topic_id_digicoach)
+        self.relation.add_tag(topic_id_digicoach, topic_version_id_digicoach, tag)
+        # self.topic.checkin(topic_id_digicoach)
+
+    # Create Task topic
+    def create_task(self, name: str, topic_id_digicoach: str, topic_version_id_digicoach: str) -> str:
+        topic_id_task = str(uuid.uuid4())
+        topicTitle = name      
+        topicTypeId = self.project.get_topic_type_id("Task")     
+        parentTopicId = topic_id_digicoach
+        parentTopicRelationTypeId = self.relation.get_relation_type_id(topic_id_digicoach, topic_version_id_digicoach, "Taak")
+        parentTopicVersionId = topic_version_id_digicoach
+        self.relation.add_topic_with_relation(self.client, topic_id_task, topicTitle, topicTypeId, parentTopicId, parentTopicRelationTypeId, parentTopicVersionId)
+        print(f"Created Task topic : {topic_id_task}")
+        topic_version_id_task = self.topic.get_topicVersionId(topic_id_task)
+        return topic_id_task, topic_version_id_task
+    
+    # Create Action topic
+    def create_step(self, name: str, topic_id_task: str, topic_version_id_task: str) -> str:
+        topic_id_step = str(uuid.uuid4())
+        topicTitle = name       
+        topicTypeId = self.project.get_topic_type_id("Action")     
+        parentTopicId = topic_id_task
+        parentTopicRelationTypeId = self.relation.get_relation_type_id(topic_id_task, topic_version_id_task, "Stap")
+        parentTopicVersionId = topic_version_id_task
+        self.relation.add_topic_with_relation(self.client, topic_id_step, topicTitle, topicTypeId, parentTopicId, parentTopicRelationTypeId, parentTopicVersionId)
+        print(f"Created Action topic : {topic_id_step}")
+        topic_version_id_step = self.topic.get_topicVersionId(topic_id_step)
+        return topic_id_step, topic_version_id_step
+    
+    def add_content_to_topic(self, topicId: str, topicVerionId: str, text: str):
+        content = self.topic.get_topic_parts(topicId=topicId)
+
+        # Selecteer part uit topic met daarin de content.
+        part = content['topicEditorData']['groups'][1]['parts'][0]
+
+        # Checkout topic.
+        result = self.topic.checkout(topicId)
+
+        # Selecteer huidig topicVersionId.
+        topicVersionId = result['topicVersionId']
+
+        # Pas content topic aan.
+        self.topic.topic_add_content(topicVersionId=topicVersionId, topicId=topicId, partId="body", part=part, new_text=f'<p>{text}</p>')
+
+        # Checkin topic.
+        self.topic.checkin(topicId)
+
+    #  Creates a workflow transition request for predefined_search topic.
+    def publiceer(self, topic_id: str):
+        request_id = self.workflow.create_workflow_transition_request(topic_id)
+        transitions_model = self.workflow.get_workflow_transition_request_transitions_model(request_id)
+        self.workflow.update_workflow_transition_request(request_id, transitions_model)
+        self.workflow.approve_workflow_transition_request(request_id)
